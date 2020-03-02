f786004bc019f40e9a0c4bb15ee5f44cec496c0f
Kat Millage <kmillage@ucsb.edu>
2020-03-02 14:05:25 -0800

    happy enough with layout of front page

--- app/ui_01_introduction.R
+++ app/ui_01_introduction.R
@@ -14,6 +14,8 @@ introduction = function()
     # open links in new window
     tags$head(tags$base(target = "_blank")),
     
+    ### Image with text -------------------
+
     # Parent container: landing page header (photo + overlay)
     tags$div(class = "landing-wrapper", style = "border-bottom: 4px solid #3c8dbc;",
              
@@ -40,92 +42,62 @@ introduction = function()
                       )
              ) # /div landing-block foreground-content
              
-    ) # /div landing-wrapper
+    ), # /div landing-wrapper
+    
+    ### Buttons -------------------
+    fluidRow(
+
+        column(12,
+               align = "center",
+
+               tags$h4("Are you familiar with fisheries subsidies?")
+
+        ),
+
+        column(12,
+               align = "center",
+
+               # Yes column
+               column(6,
+                      tags$h4("Yes. I want to..."),
+
+                      column(5, offset = 1,
+                             actionButton("ab-selected-results",
+                                          "Explore the results in more detail",
+                                          style = "color: #fff; background-color: #3c8dbc; border-color: #2e6da4; padding: 15px 10px; width:80%;")
+                      ),
+
+                      column(5,
+                             actionButton("ab-more-information",
+                                          "Learn more about the methods and process",
+                                          style="color: #fff; background-color: #3c8dbc; border-color: #2e6da4; padding: 15px 10px; width:80%;")
+
+                      )
+
+               ), # /column
+
+               # No column
+               column(6,
+                      tags$h4("No. Take me to..."),
+
+                      column(6,
+                             actionButton("ab-global-subsidies",
+                                          "Learn about fisheries subsidies",
+                                          style = "color: #fff; background-color: #3c8dbc; border-color: #2e6da4; padding: 15px 10px; width:80%;")
+                      ),
+
+                      column(6,
+                             actionButton("ab-need-help",
+                                          "Get help",
+                                          style="color: #fff; background-color: #3c8dbc; border-color: #2e6da4; padding: 15px 10px; width:80%;")
+
+                      )
+
+               ) # /column
+
+        ) # /column
+
+      ) # /fluidRow
     
   ) # /fluidPage
              
-             
-  #   
-  #   # Picture and text at the top ---
-  #   tags$div(class = "picture-container", style = "height: 50%; border-bottom: 4px solid #3c8dbc;",
-  #                    
-  #            # # Background image
-  #            # tags$img(class ="picture-fill", src = "intro-background.jpg"),
-  #            
-  #            # Black overlay with text
-  #            tags$div(class = "overlay-black",
-  #                             
-  #                     # Title and subtitle text
-  #                     tags$div(class = "overlay-text-center",
-  #                              
-  #                              
-  #                             
-  #                     ) # /div overlay-text-center
-  #                     
-  #            ) # /tags$div overlay-black
-  #            
-  #   ), # /tags$div picture-container
-  #   
-  #   # Text 
-  #   column(12, style = "padding: 20px 25px 15px;",
-  #          "text"
-  #   )
-  #   
-  #   ### Links at the bottom ---
-  #   
-  #   # fluidRow(
-  #   #   
-  #   #   column(12, 
-  #   #          align = "center",
-  #   #          
-  #   #          tags$h4("Are you familiar with fisheries subsidies?")
-  #   #          
-  #   #   ),
-  #   #   
-  #   #   column(12,
-  #   #          align = "center",
-  #   #          
-  #   #          # Yes column
-  #   #          column(6,
-  #   #                 tags$h4("Yes. I want to..."),
-  #   #                 
-  #   #                 column(5, offset = 1,
-  #   #                        actionButton("ab-selected-results",
-  #   #                                     "Explore the results in more detail",
-  #   #                                     style = "color: #fff; background-color: #3c8dbc; border-color: #2e6da4; padding: 15px 10px; width:80%;")
-  #   #                 ),
-  #   #                 
-  #   #                 column(5,
-  #   #                        actionButton("ab-more-information", 
-  #   #                                     "Learn more about the methods and process", 
-  #   #                                     style="color: #fff; background-color: #3c8dbc; border-color: #2e6da4; padding: 15px 10px; width:80%;")
-  #   #                        
-  #   #                 )
-  #   #                 
-  #   #          ), # /column
-  #   #          
-  #   #          # No column
-  #   #          column(6,
-  #   #                 tags$h4("No. Take me to..."),
-  #   #                 
-  #   #                 column(6,
-  #   #                        actionButton("ab-global-subsidies",
-  #   #                                     "Learn about fisheries subsidies",
-  #   #                                     style = "color: #fff; background-color: #3c8dbc; border-color: #2e6da4; padding: 15px 10px; width:80%;")
-  #   #                 ),
-  #   #                 
-  #   #                 column(6,
-  #   #                        actionButton("ab-need-help", 
-  #   #                                     "Get help", 
-  #   #                                     style="color: #fff; background-color: #3c8dbc; border-color: #2e6da4; padding: 15px 10px; width:80%;")
-  #   #                        
-  #   #                 )
-  #   #                 
-  #   #          ) # /column
-  #   #          
-  #   #   ) # /column
-  #   #   
-  #   # ) # /fluidRow
-  #   
-  # ) # /fluidPage
-  #   

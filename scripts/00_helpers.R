59cf29332291269caa2c668f79b7ebd4a9f5a9fa
Kat Millage <kmillage@ucsb.edu>
2020-03-13 10:15:34 -0700

    moving all data and analysis over to this repo

---
+++ scripts/00_helpers.R
@@ -0,0 +1,6 @@
+
+### -----------------------------------------------------------------
+### This script contains helpers for the SubsidyExplorer analysis  
+### Last updated on January 23, 2020
+### -----------------------------------------------------------------
+

68aa420bc59c6cc93d57f5b58e5331aa8b22c18e
Kat Millage <kmillage@ucsb.edu>
2020-09-27 13:41:55 -0700

    download buttons

--- app/ui_02a_explore_results.R
+++ app/ui_02a_explore_results.R
@@ -138,20 +138,25 @@ ExploreResults = function(proposal_choices)
                                               ),
                                               
                                        tags$td(id = "compare-fishery-stats-table-cell-2",
-                                                      
-                                               tags$button(id = "db_explore_results_download_data",
-                                                           class = "btn action-button rounded-button-grey download-button",
-                                                           tags$b(icon("external-link-alt"),
-                                                                  text$item_label[text$item_id == "db_explore_results_download_data"]))
+                                               
+                                               downloadButton("db_explore_results_download_data",
+                                                              text$item_label[text$item_id == "db_explore_results_download_data"])
+                                               
+                                               # tags$button(id = "db_explore_results_download_data",
+                                               #             class = "btn action-button rounded-button-grey download-button",
+                                               #             tags$b(icon("external-link-alt"),
+                                               #                    text$item_label[text$item_id == "db_explore_results_download_data"]))
                                                
                                        ),
                                               
                                        tags$td(id = "compare-fishery-stats-table-cell-3",
                                                       
-                                               tags$button(id = "db_explore_results_download_figure",
-                                                           class = "btn action-button rounded-button-grey download-button",
-                                                           tags$b(icon("download"),
-                                                                  text$item_label[text$item_id == "db_explore_results_download_figure"]))
+                                               downloadButton("db_explore_results_download_figure",
+                                                              text$item_label[text$item_id == "db_explore_results_download_figure"])
+                                               # tags$button(id = "db_explore_results_download_figure",
+                                               #             class = "btn action-button rounded-button-grey download-button",
+                                               #             tags$b(icon("download"),
+                                               #                    text$item_label[text$item_id == "db_explore_results_download_figure"]))
                                                       
                                        )
                                )
